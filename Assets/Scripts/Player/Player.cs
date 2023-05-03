f133eb9a40e300abf743d4702dbcbf5340e1e48e
신동빈 <shindongbeen@gmail.com>
2023-05-03 15:57:45 +0900

    playerMove

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,10 +9,14 @@ public class Player : Entity
         public float invincTime;
         public float invincDelay;
     }
-
     [SerializeField]
     private PlayerStatus playerStatus;
 
+    Rigidbody2D rigid;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
     private void Start()
     {
         base.entityStatus.maxHealth = 10;
@@ -20,17 +24,19 @@ public class Player : Entity
         base.entityStatus.walkSpeed = 5;
         base.entityStatus.canFly = true;
     }
-
-    void Update()
+    private void Update()
     {
 
-        Move();
     }
-    void Move()
+    private void FixedUpdate()
+    {
+        PlayerMove();
+    }
+    void PlayerMove()
     {
         float h, v;
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
-        transform.position += new Vector3(h, v, 0);
+        rigid.velocity = new Vector2(h, v).normalized * base.entityStatus.walkSpeed;
     }
 }
