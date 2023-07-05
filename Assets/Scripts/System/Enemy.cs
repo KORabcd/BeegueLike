ec5d1a4e0ab7799ef925d7064e0e0265b879122d
sqrtpie <mathpiegeo@gmail.com>
2023-07-05 20:39:13 +0900

    drop item shit

---
+++ Assets/Scripts/System/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+public class Enemy : Entity
+{
+    [System.Serializable]
+    public struct DropItem
+    {
+        public Item item;
+        public float percentage;
+    }
+
+    [SerializeField]
+    public DropItem[] dropItems;
+
+}
